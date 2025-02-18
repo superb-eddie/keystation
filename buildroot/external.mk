c6ce74acf6fc0608178933d5779fbcf77c7d3223
Edison Moreland <edison@devdude.net>
2025-02-17 16:03:57 -0800

    Improve caching

--- buildroot/external.mk
+++ buildroot/external.mk
@@ -1,7 +1,7 @@
 define POST_RSYNC_CARGO_VENDOR
      cd $(@D) && \
      mkdir -p .cargo/ &&\
-     PATH=$(HOST_DIR)/bin:$(PATH) $(PKG_CARGO_ENV) cargo vendor --locked VENDOR > .cargo/config
+     PATH=$(HOST_DIR)/bin:$(PATH) $(PKG_CARGO_ENV) cargo vendor --versioned-dirs --locked ${KSB_CARGO_VENDOR_DIR} > .cargo/config.toml
 endef
 
 include $(sort $(wildcard $(BR2_EXTERNAL_KBS_PATH)/package/*/*.mk))
